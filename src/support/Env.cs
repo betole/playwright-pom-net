79e74179c9edc306bae1c1b0ff5985790bfbf4d3
betole <c.mcbottle@gmail.com>
2024-06-19 22:33:28 -0300

    Init: Test Data Fixture support

---
+++ src/support/Env.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace testFramework.support;
+
+public static class Env {
+    private static IConfigurationRoot? env {get; set;}
+
+    public static void Load() {
+      Env.env = new ConfigurationBuilder()
+        .AddJsonFile("playwright.env.json")
+        .Build();
+    }
+
+    public static string? Get(string envVar) {
+        if (Env.env is null) {
+          throw new NullReferenceException("Env object has not been loaded from JSON");
+        }
+        IConfigurationSection section = Env.env.GetRequiredSection(envVar);
+        return section.Value;
+    }
+}
---
+++ src/support/Paths.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+using static NUnit.Framework.TestContext;
+
+namespace testFramework.support;
+
+public static class Paths {
+  public static string Pwd { get; } = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)!;
+  public static string ScreenshotsFolder { get; } = Path.Combine(Paths.Pwd, "reports", "screenshots");
+  public static string ScreenshotFile(TestAdapter testAdapter, bool fullPath = true) {
+    string name = testAdapter.FullName + ".png";
+    return fullPath? Path.Combine(Paths.ScreenshotsFolder, name) : name;
+  }
+}

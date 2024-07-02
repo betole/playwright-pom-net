50478bddc5109c398673a7f28cb352e9572d2180
betole <c.mcbottle@gmail.com>
2024-07-01 23:01:23 -0300

    refactor Env class -> combine sources

--- src/support/Env.cs
+++ src/support/Env.cs
@@ -7,20 +7,42 @@ public static class Env
 {
   private static IConfigurationRoot? env { get; set; }
 
+  /// <summary>
+  /// <para> Loads env vars for the test run with the following value precedence </para>
+  /// <list type="number">
+  /// <item><description> Values coming from process/cli environment </description></item>
+  /// <item><description> Values coming playwright.env.json </description></item>
+  /// <item><description> Values coming from fixture file (fixtures/{env}/config.json) </description></item>
+  /// </list>
+  /// </summary>
   public static void Load()
   {
-    Env.env = new ConfigurationBuilder()
+    //Need to build first a temporary IConfigurationRoot to determine passed ENVIRONMENT
+    IConfigurationRoot EnvWithoutFixtures = new ConfigurationBuilder()
       .AddJsonFile("playwright.env.json")
+      .AddEnvironmentVariables("PLAYWRIGHT_")
+      .Build();
+
+    String environment = EnvWithoutFixtures.GetRequiredSection("ENVIRONMENT").Value;
+
+    //With ENVIRONMENT now config from Fixtures can be included too
+    Env.env = new ConfigurationBuilder()
+      .AddJsonFile($"resources/fixtures/{environment}/config.json")
+      .AddJsonFile("playwright.env.json")
+      .AddEnvironmentVariables("PLAYWRIGHT_")
       .Build();
   }
 
   public static string? Get(string envVar)
   {
-    if (Env.env is null)
-    {
-      throw new NullReferenceException("Env object has not been loaded from JSON");
+    try {
+      return env!.GetRequiredSection(envVar).Value;
+    } catch (InvalidOperationException e) {
+      throw new NullReferenceException(
+        $"Env var '{envVar}' not found. Was Env loaded correctly first?", e
+      );
+    } catch {
+      throw;
     }
-    IConfigurationSection section = Env.env.GetRequiredSection(envVar);
-    return section.Value;
   }
 }
